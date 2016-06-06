85f8fd4a7389b79d8fb558cd1cb4d90a97bea8e7
talhaobject90 <talhaqqq@gmail.com>
2016-06-06 11:14:31 +0530

    after fixing enqur-form-new

--- app/models/followup.js
+++ app/models/followup.js
@@ -2,10 +2,10 @@ import DS from 'ember-data';
 // import Ember from 'ember';
 
 export default DS.Model.extend({
-  
+
 date: DS.attr('date'),
 message: DS.attr('string'),
 followupdate: DS.attr('date'),
-employee: DS.belongsTo('enquiry' ,{async:true}),
+enquiry: DS.belongsTo('enquiry' ,{async:true}),
 
 });
