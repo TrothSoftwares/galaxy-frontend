3c82ae1b789a0c571c723dbcd490612e4d3119d9
talhaobject90 <talhaqqq@gmail.com>
2016-06-06 12:23:16 +0530

    after adding edit -view forms

--- app/routes/dashboard/enquiry-forms/enquiry-form/edit.js
+++ app/routes/dashboard/enquiry-forms/enquiry-form/edit.js
@@ -1,4 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+
+
+
+  model: function() {
+    return Ember.RSVP.hash({
+    enquiry: this.modelFor('dashboard.enquiry-forms.enquiry-form'),
+  });
+
+  },
+
+  setupController: function(controller ,model) {
+    controller.set('enquiry',model.enquiry);
+  }
 });
