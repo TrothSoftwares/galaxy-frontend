124d0c66e55e783f078724d414c961b1f35d7556
talhaobject90 <talhaqqq@gmail.com>
2016-06-06 13:57:45 +0530

    after filtering pending enquiries

--- app/routes/dashboard/enquiry-forms/index.js
+++ app/routes/dashboard/enquiry-forms/index.js
@@ -5,12 +5,18 @@ export default Ember.Route.extend({
   model: function() {
 
     return Ember.RSVP.hash({
-      enquiries: this.store.findAll('enquiry' ,{reload :true})
-    });
+     enquiries: this.store.findAll('enquiry' ,{reload: true}).then(function(enquiries){
+          return {
+            pendingEnquiries:  enquiries.filterBy('status', 'Pending')
+        };
+     }),
+   });
   },
 
-  setupController: function(controller,model) {
-    controller.set('enquiries',model.enquiries);
-  },
+  setupController: function(controller ,model) {
 
+      controller.set('enquiries',model.enquiries);
+      controller.set('pendingEnquiries',model.enquiries.pendingEnquiries);
+
+  }
 });
