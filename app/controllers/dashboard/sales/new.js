70802842cc8b9af80570e57e8767f7ff895bd6de
talhaobject90 <talhaqqq@gmail.com>
2016-06-07 18:25:25 +0530

    after enable customer

--- app/controllers/dashboard/sales/new.js
+++ app/controllers/dashboard/sales/new.js
@@ -4,15 +4,76 @@ export default Ember.Controller.extend({
 
   enableEnquiryCustomer: false,
   enableNewCustomer :false,
+  enableCustomer :false,
+
+  iscreateSaleButtonEnabled: Ember.computed( 'totalprice'   ,  function() {
+    if( Ember.isEmpty(this.get('totalprice'))
+  ){return 'disabled';}
+  else{return '';}
+  }),
 
   actions:{
     enableEnquiryCustomer:function(){
       this.set('enableNewCustomer', false);
+      this.set('enableCustomer', false);
       this.toggleProperty('enableEnquiryCustomer');
+    },
+    enableCustomer:function(){
+      this.set('enableEnquiryCustomer', false);
+      this.set('enableNewCustomer', false);
+      this.toggleProperty('enableCustomer');
     },
     enableNewCustomer:function(){
       this.set('enableEnquiryCustomer', false);
+      this.set('enableCustomer', false);
       this.toggleProperty('enableNewCustomer');
     },
+
+
+    createSale:function(){
+
+      var controller =this;
+      var name,address,contact,email;
+
+      if(this.get('enableEnquiryCustomer') === true){
+        name = this.get('enquirycustomer.name');
+        address = this.get('enquirycustomer.address');
+        contact = this.get('enquirycustomer.contact');
+        email = this.get('enquirycustomer.email');
+      }
+      if(this.get('enableNewCustomer') === true){
+        name = this.get('newcustomername');
+        address = this.get('newcustomeraddress');
+        contact = this.get('newcustomercontact');
+        email = this.get('newcustomeremail');
+    }
+
+    var customer = controller.store.createRecord('customer', {
+        name :name,
+        address :address,
+        contact :contact,
+        email :email
+      });
+
+      customer.save().then(function(){
+
+        var sale = controller.store.createRecord('sale', {
+            totalprice :controller.get('totalprice'),
+            installpricepermonth : controller.get('installpricepermonth'),
+            months :controller.get('months'),
+            customer :customer
+          });
+
+          sale.save().then(function(){
+            controller.notifications.addNotification({
+            message: 'Saved!' ,
+            type: 'success',
+            autoClear: true
+          });
+          controller.transitionToRoute('dashboard.sales.index');
+          });
+
+      });
+    }
   }
 });
